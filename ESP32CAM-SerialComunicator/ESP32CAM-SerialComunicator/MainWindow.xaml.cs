40098c0203d7d3d4b4e34512b76ee5f6a2d4f4be
Nikola Radojcic <nikolajazak@gmail.com>
2023-04-05 11:47:51 +0200

    Dodat predjasnje uradjen deo

---
+++ ESP32CAM-SerialComunicator/ESP32CAM-SerialComunicator/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace ESP32CAM_SerialComunicator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static bool _continue;
+        static SerialPort _serialPort;
+        private delegate void SetTextDeleg(string text);
+        private delegate void SetImageDeleg(byte[] bytes);
+        private void si_DataReceived(string data) { textBox1.Text = data.Trim(); }
+        private void img_DataReceived(byte[] decodedBytes) { image1.Source = LoadImage(decodedBytes);  }
+
+        public MainWindow()
+        {
+            _serialPort = new SerialPort("COM8", 9600, Parity.None, 8, StopBits.One);
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+            _serialPort.Open();
+            InitializeComponent();
+        }
+
+        void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Thread.Sleep(500);
+            string data = _serialPort.ReadLine();
+            // Invokes the delegate on the UI thread, and sends the data that was received to the invoked method.
+            // ---- The "si_DataReceived" method will be executed on the UI thread, which allows populating the textbox.
+            this.Dispatcher.BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { data });
+            //byte[] decodedBytes = Convert.FromBase64String(data);
+            //this.Dispatcher.BeginInvoke(new SetImageDeleg(img_DataReceived), new object[] { decodedBytes });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!(_serialPort.IsOpen))
+                    _serialPort.Open();
+                _serialPort.Write("1\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening/writing to serial port :: " + ex.Message, "Error!");
+            }
+        }
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+
+    }
+}
---
+++ sketch_mar28a.ino
@@ -0,0 +1,102 @@
+#include "esp_camera.h"
+#include "Arduino.h"
+#include "soc/soc.h"           // Disable brownour problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownour problems
+#include "driver/rtc_io.h"
+#include "mbedtls/base64.h"
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+
+
+int timer = 0; 
+camera_fb_t * fb = NULL;
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  Serial.begin(9600);
+
+    camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // Init Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void loop() {
+
+  if(Serial.available()) {
+      String command = Serial.readStringUntil('\n');
+      if (command == "1"){
+        delay(1000);
+        fb = esp_camera_fb_get();  
+        if(!fb) {
+          Serial.println("Camera capture failed");
+          return;
+        }
+        
+        unsigned char output[fb->len];
+        size_t outlen;
+        mbedtls_base64_encode(output, 64, &outlen, fb->buf, fb->len);
+        for(int i = 0; i<fb->len; i++){
+          Serial.println(fb->buf[i]);
+        }
+        Serial.println("");
+        esp_camera_fb_return(fb); 
+        // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
+        pinMode(4, OUTPUT);
+        digitalWrite(4, LOW);
+        rtc_gpio_hold_en(GPIO_NUM_4);
+        
+      }
+  }
+}
